f09849267cc50faf21c608b9d0afbed71f014d8e
Salman Manoe <salmanoe@live.com>
2025-02-15 21:19:56 +0700

    Configuration update
    Implement spring profile

--- security/src/main/java/com/github/ultimattern/security/SecurityApplication.java
+++ security/src/main/java/com/github/ultimattern/security/SecurityApplication.java
@@ -2,8 +2,10 @@ package com.github.ultimattern.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = "file:./.env")
 public class SecurityApplication {
 
     public static void main(String[] args) {
